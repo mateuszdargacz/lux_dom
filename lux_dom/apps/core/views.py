339a4da12ef09b06750f4914ac6c30bae78afc96
pro <pro@Mateuszek.local>
2015-07-01 18:31:32 +0200

    basics

--- lux_dom/apps/core/views.py
+++ lux_dom/apps/core/views.py
@@ -7,24 +7,29 @@ from django.shortcuts import render
 
 def main(request):
     context = {}
+    context.update(active='main')
     return render(request, 'core/main.html', context=context)
 
 
 def houses(request):
     context = {}
+    context.update(active='houses')
     return render(request, 'core/houses.html', context=context)
 
 
 def hood(request):
     context = {}
+    context.update(active='hood')
     return render(request, 'core/hood.html', context=context)
 
 
 def gallery(request):
     context = {}
+    context.update(active='gallery')
     return render(request, 'core/gallery.html', context=context)
 
 
 def contact(request):
     context = {}
+    context.update(active='contact')
     return render(request, 'core/contact.html', context=context)
